e989d57bed911b131b9a48220e9153bc5476309e
sou7hernsaint <sou7hernsaint@gmail.com>
2021-12-25 16:40:52 -0500

    init for html & css

--- calculatorApp.js
+++ calculatorApp.js
@@ -1,1 +1,33 @@
 // Calculator App (My attempt)
+
+// Attempt 1: Create an outer "calculator" function, with inner mathematical functions
+// Result(s): Cannot call innter fucntions due to "closure"
+// function calculator(input) {
+//     const addition = function (number1) {
+//         console.log(number1);
+//         // console.log(typeOf number1);
+//     };
+
+// }
+// console.log(calculator(addition(5)));
+
+// Attempt 2: Create outer functios of each mathematical function
+// Result(s): 
+function addition(input1, input2) {
+    console.log(`This is input1: ${input1}`);
+    console.log(`This is input1: ${input2}`);
+    // console.log(typeof input);
+
+    // result = (input1.parseInt + input2.parseInt);
+    result = input1 + input2;
+
+    return result
+}
+console.log(addition(5, 6));
+
+// function addition(input) {
+//     console.log(input);
+//     console.log(typeof input);
+//     return input
+// }
+// console.log(addition(5));
